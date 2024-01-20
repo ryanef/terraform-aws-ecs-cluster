e5cde97dbb811129135ed4259960b3863f63bba8
ryan-wsl <ryanef39@gmail.com>
2024-01-20 13:53:35 -0500

    outputs

--- output.tf
+++ output.tf
@@ -1,8 +1,8 @@
 output "cluster_id" {
-  value = try({for k,v in aws_ecs_cluster.cluster : k => v.id}, {})
+  value = try({for k,v in aws_ecs_cluster.cluster : k => v.id}, aws_ecs_cluster.cluster.id)
   }
 output "cluster_arn" {
-  value = try({for k,v in aws_ecs_cluster.cluster : k => v.arn}, {})
+  value = try({for k,v in aws_ecs_cluster.cluster : k => v.arn}, aws_ecs_cluster.cluster.arn)
 }
 
 output "namespace_id" {
